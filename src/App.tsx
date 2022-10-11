4d6897c5bb759d6d479aa934e96f07b5fe6a84c6
Vincent Abesamis <vincentabesamis03@gmail.com>
2022-10-11 08:49:18 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="mx-auto w-3/5 py-8">
+      <h1 className="text-3xl font-bold text-center">Robodeck</h1>
+    </main>
+  );
+}
+
+export default App;
